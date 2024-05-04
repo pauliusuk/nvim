0eecd590a475dcebfbd2474ee8f5b1cd40831aec
paulobox <plsjnts37@gmail.com>
2024-05-04 22:56:46 +0100

    updt

--- init.lua
+++ init.lua
@@ -4,4 +4,3 @@ require("core.autocmds")
 require("core.keymaps")
 require("core.set")
 require("current-theme")
-vim.g.python3_host_prog = '%userprofile%\\AppData\\Local\\Microsoft\\WindowsApps\\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\\python.exe'
